67b1f45237e1ba6c3c0fa2d237f229e380a89de6
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-03-05 12:18:32 +0200

    Update demo.js

--- JS intro/demo.js
+++ JS intro/demo.js
@@ -1,8 +1,11 @@
 /* alert("hello");
 alert("world"); */
-alert(typeof("asd"));
+/* alert(typeof("asd"));
 alert(typeof(123));
 alert(typeof(false));
 alert(typeof(1.2));
+ */
+var my_name = prompt('what is your name?');
+alert('hello ' + my_name + ' welcome ' + my_name + ' home');
 
 
