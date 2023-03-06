89afd17badb5c297d7958e86eec28544b15f954b
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-03-06 09:26:55 +0200

    Update demo.js

--- JS intro/demo.js
+++ JS intro/demo.js
@@ -24,9 +24,19 @@ alert('hello ' + my_name + ' welcome ' + my_name + ' home');
 /* var my_name = prompt('enter your name here: ');
 alert('your name has ' + my_name.length + ' characters in it!'); */
 
-var tweet_limit = 180
+/* var tweet_limit = 10
 var msg = prompt('enter your tweet here: ')
 var char_left = tweet_limit - msg.length
-alert('your tween is ' + msg.length + ' characters long! You have left ' + char_left + ' charaters left out of ' + tweet_limit)
+alert('your tween is ' + msg.length + ' characters long! You have ' + char_left + ' charaters left out of ' + tweet_limit)
+alert('so this is what is left of your tweet: ' + msg.slice(0, tweet_limit)) */
 
+var msg = prompt('enter your name here: ');
+var first_letter = msg.split(1, 2);
+first_letter = first_letter.toUpperCase();
+alert(first_letter);
+var remaining_name = msg.split(2, 100);
+alert(remaining_name);
+var propper_name = first_letter + remaining_name;
+alert(propper_name);
+alert('Hello ' + propper_name);
 
