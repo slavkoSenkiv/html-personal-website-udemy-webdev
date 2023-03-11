6306045ad2770e07d02b74ec8264c244bca21749
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-03-11 16:00:32 +0200

    Main

--- DOM/index.js
+++ DOM/index.js
@@ -1,1 +1,4 @@
-document.querySelector("h1").innerHTML = "bye";
+/* document.querySelector("h1").innerHTML = "bye"; */
+var headign = document.firstElementChild.lastElementChild.firstElementChild;
+headign.innerHTML = 'lol';
+headign.style.color = 'green';
