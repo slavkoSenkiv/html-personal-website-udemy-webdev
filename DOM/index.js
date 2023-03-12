ec75e3b2bf180dcdd6bfee211a3d769cd06abc18
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-03-12 09:30:11 +0200

    upd

--- DOM/index.js
+++ DOM/index.js
@@ -1,4 +1,7 @@
-/* document.querySelector("h1").innerHTML = "bye"; */
-var headign = document.firstElementChild.lastElementChild.firstElementChild;
-headign.innerHTML = 'lol';
-headign.style.color = 'green';
+var heading = document.firstElementChild.lastElementChild.firstElementChild;
+heading.innerHTML = "bye";
+document.querySelector("h1").style.color = "green"; 
+document.getElementsByTagName('li')[2].innerHTML = 3;
+document.getElementsByClassName('list')[1].innerHTML = 2;
+document.getElementById('link').style.color = 'red';
+document.querySelector('#link').innerHTML = 'bing';
