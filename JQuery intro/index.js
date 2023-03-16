0f82b3495e4c056c9a2e1c06dd00f2975024174b
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-03-16 10:03:09 +0200

    upd

---
+++ JQuery intro/index.js
@@ -0,0 +1,14 @@
+/* document.querySelector('h1').style.fontSize = '50px';
+ */
+/* $(document).ready(function(){
+    $('h1').css('color', 'green');
+})
+ */
+
+/* $('h1').css('color', 'green');
+ */
+
+$('h1').addClass('big-title margin-around ');
+
+/* $('button').text('hey');
+ */$('button').html('<em> bye <em>');
