7bc5dbb249058f6536364a53193102d04efb1946
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-03-17 08:42:48 +0200

    Update index.js

--- Simon Game/index.js
+++ Simon Game/index.js
@@ -1,5 +1,47 @@
-$('button').click(function(e){
-    var btnColor = e.attr('id')
-    var sound = new Audio('sounds/' + btnColor + '.mp3');
-    sound.play();
-})
+function userPlaySound(){
+    $('div[type=button]').click(function(){
+        var btnColor = $(this).attr('id');
+        var sound = new Audio('sounds/' + btnColor + '.mp3');
+        sound.play();
+        });
+};
+
+function userClickEffect(){
+    $('div[type=button]').click(function(){
+        var btn = $(this);
+        btn.addClass('pressed');
+        setTimeout(function(){btn.removeClass('pressed');}, 100);
+    });
+};
+
+function systemIndicator(color){
+    var picker = 'div[id=' + color + ']'
+    $(picker).click(function(){
+        var btnColor = $(this).attr('id');
+        var sound = new Audio('sounds/' + btnColor + '.mp3');
+        sound.play();
+        });
+
+    $(picker).click(function(){
+        var btn = $(this);
+        btn.addClass('pressed');
+        setTimeout(function(){btn.removeClass('pressed');}, 100);
+    });
+}
+
+userPlaySound();
+userClickEffect();
+
+
+var btnArr = ['green', 'red', 'yellow', 'blue'];
+var gamePattern = [];
+
+function nextSequence(){
+    var randNum = Math.floor(Math.random()*4);
+    var randColor = btnArr[randNum];
+    gamePattern.push(randColor);
+    
+}
+
+systemIndicator('green');
+
