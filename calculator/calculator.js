93159942b9774449d7a3d5fdbcdc70b68822e1ef
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-03-20 21:22:43 +0200

    upd

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({extended:true}));
+
 /* app.get('/', function(req, res){
     res.send('<h1>hello there1</h1>');
 });
@@ -9,10 +11,19 @@ app.get('/sidepage', function(req, res){
 });
 app.get('/thirdpage', function(req, res){
     res.send('<h1>third page</h1>');
+    console.log(__dirname, __filename);
+
 }); */
+
 app.get('/', function(req, res){
-    /* console.log(__dirname, __filename); */
     res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function(req, res){
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+    res.send(' the result is ' + result);
 });
 
 app.listen(3000, function(){
