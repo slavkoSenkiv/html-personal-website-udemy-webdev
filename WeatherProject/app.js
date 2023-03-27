c80deaa0d2b6892ab0efecb2e0d7b2fc1adb8b6b
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-03-27 21:39:24 +0300

    upd

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const https = require("https");
 
 app.get('/', function(req, res){
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=49&lon=23&appid=abd9dd7f7b314d3b1281bccb7e2a1e44';
+    https.get(apiUrl, function())
+
     res.send('server is up and running!');
 })
 
