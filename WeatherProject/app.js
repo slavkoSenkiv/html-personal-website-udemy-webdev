bb981500540683cbbfd58ad60e69536a6004f847
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-03-28 21:40:12 +0300

    upd

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -4,11 +4,21 @@ const https = require("https");
 
 app.get('/', function(req, res){
     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=49&lon=23&appid=abd9dd7f7b314d3b1281bccb7e2a1e44';
-    https.get(apiUrl, function())
+    https.get(apiUrl, function(responce){
+        console.log(responce.statusCode);
 
-    res.send('server is up and running!');
+        responce.on('data', function(data){
+            console.log(data);
+            var jsonData = JSON.parse(data);
+            var temp = jsonData.main.temp;
+            console.log(jsonData);
+            console.log(temp);
+            console.log(jsonData.weather[0].description)
+        });
+    })
+
+    res.send('server is up and runninng!');
 })
-
 app.listen(3000, function(){
     console.log("server is listening to port 3000");
 })
