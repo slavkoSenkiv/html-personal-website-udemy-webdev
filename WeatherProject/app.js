7ec05c53651b9dce6280959a3dfaa78fdd650a22
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-04-01 20:23:07 +0300

    upd

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -3,23 +3,33 @@ const app = express();
 const https = require("https");
 
 app.get('/', function(req, res){
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=49&lon=23&appid=abd9dd7f7b314d3b1281bccb7e2a1e44';
-    https.get(apiUrl, function(responce){
-        console.log(responce.statusCode);
-
-        responce.on('data', function(data){
-            var jsonData = JSON.parse(data);
-            console.log(jsonData);
-            var temp = jsonData.main.temp;
-            var weather = jsonData.weather[0].description;
-            var icon = ' https://openweathermap.org/img/wn/' + jsonData.weather[0].icon + '@2x.png';
-            res.write('<h1>the temperature is ' + temp + '</h1>');
-            res.write('<p>and the weather is ' + weather + '<p>');
-            res.write('<img src="' + icon + '">');
-            res.send();
-        });
-    });
+    res.sendFile(__dirname + '/index.html');
 });
+
+app.post('/', function(req, res){
+    console.log('post received');
+})
+
 app.listen(3000, function(){
     console.log("server is listening to port 3000");
 })
+
+// var city = 'Drohobych';
+//     var apiKey = 'abd9dd7f7b314d3b1281bccb7e2a1e44';
+//     var unit = 'metric';
+//     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey + '&units=' + unit;
+//     https.get(apiUrl, function(responce){
+//         console.log(responce.statusCode);
+
+//         responce.on('data', function(data){
+//             var jsonData = JSON.parse(data);
+//             console.log(jsonData);
+//             var temp = jsonData.main.temp;
+//             var weather = jsonData.weather[0].description;
+//             var icon = ' https://openweathermap.org/img/wn/' + jsonData.weather[0].icon + '@2x.png';
+//             res.write('<h1>the temperature is ' + temp + '</h1>');
+//             res.write('<p>and the weather is ' + weather + '<p>');
+//             res.write('<img src="' + icon + '">');
+//             res.send();
+//         });
+//     });
